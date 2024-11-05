cd6600aa9df0985f913996ee8ccedf3a60892ae7
nibezo <nibezo.cs@gmail.com>
2024-11-05 14:13:40 +0400

    Добавил сортировку заказов в админку

--- src/components/pages/adminPages/OrdersPage/OrdersPage.tsx
+++ src/components/pages/adminPages/OrdersPage/OrdersPage.tsx
@@ -1,26 +1,49 @@
-import Actions from '../../../layouts/adminLayouts/Actions/Actions';
-import Content from '../../../layouts/adminLayouts/Content/Content';
-import { useSelector } from 'react-redux';
-import { RootState } from '../../../../store/store';
-import Order from '../../../admin/Order/Order';
-import Card from '../../../UI/Card/Card';
-import Loader from '../../../UI/Loader/Loader';
-import Placeholder from '../../../UI/Placeholder/Placeholder';
-import { NO_ORDERS } from '../../../../constants/messages';
+import Actions from "../../../layouts/adminLayouts/Actions/Actions";
+import Content from "../../../layouts/adminLayouts/Content/Content";
+import { useSelector } from "react-redux";
+import { RootState } from "../../../../store/store";
+import Order from "../../../admin/Order/Order";
+import { Order as OrderTypes } from "../../../../types/common";
+import Card from "../../../UI/Card/Card";
+import Loader from "../../../UI/Loader/Loader";
+import Placeholder from "../../../UI/Placeholder/Placeholder";
+import { NO_ORDERS } from "../../../../constants/messages";
+import { useEffect, useState } from "react";
+import classes from "../../../UI/BaseLink/BaseLink.module.css";
 
 const OrdersPage: React.FC = () => {
   const { orders, isLoading } = useSelector((state: RootState) => state.common);
+  const [sortType, setSortType] = useState("old");
+  const [ordersCopy, setOrdersCopy] = useState<OrderTypes[]>();
+
+  const changeSortType = () => {
+    setSortType(sortType === "old" ? "new" : "old");
+    setOrdersCopy(ordersCopy?.reverse());
+  };
+
+  useEffect(() => {
+    if (orders.length !== 0) {
+      setOrdersCopy([...orders]);
+    }
+  }, [orders]);
 
   return (
     <>
-      <Actions title={'Заказы'} />
-
+      <Actions title={"Заказы"} />
       <Content>
         <>
           {isLoading && <Loader />}
           {!isLoading && (
             <Card fullWidth>
               <>
+                <button
+                  className={`${classes.link} ${classes.button} ${classes["m"]}`}
+                  onClick={changeSortType}
+                >
+                  {sortType === "old"
+                    ? "Сначала старые заказы ↓"
+                    : "Сначала новые заказы ↑"}
+                </button>
                 <table>
                   <thead>
                     <tr>
@@ -40,17 +63,27 @@ const OrdersPage: React.FC = () => {
                       </tr>
                     )}
 
-                    {orders.map(({ user, totalPrice, totalWeight, totalDiscount, cart, timestamp, id }) => (
-                      <Order
-                        user={user}
-                        cart={cart}
-                        totalPrice={totalPrice}
-                        timestamp={timestamp}
-                        key={id}
-                        totalWeight={totalWeight}
-                        totalDiscount={totalDiscount}
-                      />
-                    ))}
+                    {ordersCopy?.map(
+                      ({
+                        user,
+                        totalPrice,
+                        totalWeight,
+                        totalDiscount,
+                        cart,
+                        timestamp,
+                        id,
+                      }) => (
+                        <Order
+                          user={user}
+                          cart={cart}
+                          totalPrice={totalPrice}
+                          timestamp={timestamp}
+                          key={id}
+                          totalWeight={totalWeight}
+                          totalDiscount={totalDiscount}
+                        />
+                      )
+                    )}
                   </tbody>
                 </table>
               </>
